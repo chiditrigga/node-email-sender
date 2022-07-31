ff7002f6a0b821db1b3c528e9d07a4625cce9498
chiditrigga <105122478+chiditrigga@users.noreply.github.com>
2022-07-31 23:51:37 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+var nodemailer = require('nodemailer');
+
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'chididaniel1998@gmail.com',
+    pass: 'fypwulznfjjsbana'
+  }
+});
+
+var mailOptions = {
+  from: 'chididaniel1998@gmail.com',
+  to: 'chididaniel1998@gmail.com',
+  subject: 'Sending Email using Node.js',
+  text: 'That was easy!'
+};
+
+transporter.sendMail(mailOptions, function(error, info){
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Email sent: ' + info.response);
+  }
+});
